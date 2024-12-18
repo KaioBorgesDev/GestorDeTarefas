9d26cfdec5ea882a168d8b7b8dfa5667b164ee56
KaioBorgesDev <kaioeduardo401@outlook.com>
2024-12-18 10:43:18 -0300

    Schema com o mongoose da TASK

---
+++ src/infra/model/TaskModel.ts
@@ -0,0 +1,19 @@
+import { Schema, model } from "mongoose";
+import crypto from 'node:crypto';
+
+const TaskSchema = new Schema({
+    uuid: { type: String, default: crypto.randomUUID },
+    id_user: { type: String, required: true },
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    created_At: { type: Date, default: Date.now() },
+    status: {
+        type: String,
+        enum: ["Pendente", "Concluída"],
+        default: "Concluída",
+    },
+});
+
+const TaskModel = model("task", TaskSchema);
+
+export default TaskModel;
