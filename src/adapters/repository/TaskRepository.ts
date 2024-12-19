11f787e1f0ebba0799d35c81df86cab2bbd3ebc6
KaioBorgesDev <kaioeduardo401@outlook.com>
2024-12-19 10:08:51 -0300

    close task implementado.

--- src/adapters/repository/TaskRepository.ts
+++ src/adapters/repository/TaskRepository.ts
@@ -4,7 +4,8 @@ import { Task } from "src/entitie/Task";
 export default interface TaskRepository {
     save(id_user: UUID, title: String, description: String): Promise<void>;
     update(task: Task): Promise<void>;
-    remove(uuid_task: String): Promise<void>;
+    remove(uuid_task: UUID): Promise<void>;
+    close(uuid_task: UUID): Promise<void>;
     get(uuid_task: String): Promise<Task | null>;
     getAll(): Promise<Task[]>;
     listByStatus(status: "Pendente" | "Concluída"): Promise<Task[]>;
